58dac27c8fb06849506340bd6bdb123d36d278cc
Wayne Graham <wgraham@clir.org>
2022-09-03 08:05:17 -0400

    Decode bytes into string

--- speedtest.py
+++ speedtest.py
@@ -8,7 +8,9 @@ import time
 response = subprocess.Popen(
     'speedtest-cli --simple --single', shell=True, stdout=subprocess.PIPE).stdout.read()
 
-ping = respons = re.findall('Ping:\s(.*?)\s', response, re.MULTILINE)
+response = response.decode('utf-8')
+
+ping = re.findall('Ping:\s(.*?)\s', response, re.MULTILINE)
 download = re.findall('Download:\s(.*?)\s', response, re.MULTILINE)
 upload = re.findall('Upload:\s(.*?)\s', response, re.MULTILINE)
 
@@ -20,8 +22,8 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 
 try:
     if os.stat(dir_path + '/speedtest.csv').st_size == 0:
-        print 'Date,Ping,Download,Upload'
+        print('Date,Ping,Download,Upload')
 except:
     pass
 
-print '{},{},{},{}'.format(time.strftime('%m-%d-%y %H:%M'), ping[0], download[0], upload[0])
+print('{},{},{},{}'.format(time.strftime('%m-%d-%y %H:%M'), ping[0], download[0], upload[0]))
