794a75493ad06bb68a59e056ce3b8d504b651e66
Wayne Graham <wgraham@clir.org>
2022-09-19 15:37:26 -0400

    Use python3 explicitly

--- speedtest-cron.sh
+++ speedtest-cron.sh
@@ -1,2 +1,2 @@
 #! /usr/bin/env bash
-python /home/pi/pi_wifi_monitor/speedtest.py >> /home/pi/pi_wifi_monitor/speedtest.csv
+python3 /home/pi/pi_wifi_monitor/speedtest.py >> /home/pi/pi_wifi_monitor/speedtest.csv
